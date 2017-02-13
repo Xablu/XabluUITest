6e33d31d3e5b0a5f4bc85bc83cd45a76bba06bc3
Maurits van Beusekom <mvanbeusekom@route2it.com>
2017-02-13 14:20:37 +0100

    Fix indentation

--- ScreenObjectXUI/Utils/AppExtensions.cs
+++ ScreenObjectXUI/Utils/AppExtensions.cs
@@ -20,20 +20,20 @@
             return app.Query(query).First();
         }
 
-		public static AppResult WaitForFirstElement(this IApp app, QueryFunc query)
-		{
-			return app.WaitForElement(query).First();
-		}
+        public static AppResult WaitForFirstElement(this IApp app, QueryFunc query)
+        {
+        	return app.WaitForElement(query).First();
+        }
 
-		public static AppResult WaitForLastElement(this IApp app, QueryFunc query)
-		{
-			return app.WaitForElement(query).Last();
-		}
+        public static AppResult WaitForLastElement(this IApp app, QueryFunc query)
+        {
+        	return app.WaitForElement(query).Last();
+        }
 
-		public static AppResult WaitForElementOnIndex(this IApp app, QueryFunc query, int index)
-		{
-			return app.WaitForElement(query)[index];
-		}
+        public static AppResult WaitForElementOnIndex(this IApp app, QueryFunc query, int index)
+        {
+        	return app.WaitForElement(query)[index];
+        }
 
         public static AppResult[] WaitForElementContainingText(this IApp app, string substring)
         {
